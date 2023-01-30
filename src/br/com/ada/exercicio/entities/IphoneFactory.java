14af2e619c349eff073e3e6306d7bfab6ce25b1b
jkmacielbr <jk.maciel.br@gmail.com>
2023-01-30 19:24:17 -0400

    ajuste na classe IphoneFactory.java

--- src/br/com/ada/exercicio/entities/IphoneFactory.java
+++ src/br/com/ada/exercicio/entities/IphoneFactory.java
@@ -4,16 +4,12 @@ public class IphoneFactory {
 
 
     public Iphone cadastrarIphone (TypeIphoneEnum typeIphone){
-        Iphone iphone;
         if (typeIphone == TypeIphoneEnum.IPHONE_9){
-            iphone = new Iphone9();
-            iphone.exibirDetalhes();
+            new Iphone9().exibirDetalhes();
         } else if (typeIphone == TypeIphoneEnum.IPHONE_X){
-            iphone = new IphoneX();
-            iphone.exibirDetalhes();
+            new IphoneX().exibirDetalhes();
         } else {
-            iphone = new Iphone13Mini();
-            iphone.exibirDetalhes();
+            new Iphone13Mini().exibirDetalhes();
         }
 
         return null;
