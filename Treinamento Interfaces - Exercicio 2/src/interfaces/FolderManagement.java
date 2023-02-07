2b258af1e825c80423f1c75e1386e4c83e0cd49a
jkmacielbr <jk.maciel.br@gmail.com>
2023-02-07 18:47:06 -0400

    ajustes repositório

--- Treinamento Interfaces - Exercicio 2/src/interfaces/FolderManagement.java
+++
@@ -1,7 +0,0 @@
-package interfaces;
-
-public interface FolderManagement {
-    void createAFolder(String path);
-    void removeAFolder(String path);
-    void listAllFoldersCreated();
-}
--- Treinamento Interfaces - Exercicio 2/src/model/MFileAnnotationType.java
+++
@@ -1,8 +0,0 @@
-package model;
-
-public enum MFileAnnotationTypeEnum {
-    REMINDER,
-    IMPORTANT,
-    SIMPLE,
-    IMAGE
-}
